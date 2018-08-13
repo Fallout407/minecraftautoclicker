d864bdf914333d80122c015ca1ce7b839fe985cc
Fallout407 <38690894+Fallout407@users.noreply.github.com>
2018-08-13 18:57:15 +0800

    Fixed some HORRID spacing

--- lib/main.cpp
+++ lib/main.cpp
@@ -16,15 +16,15 @@ int main() {
 	int range = cpsmax - cpsmin;
 	#define RAND_MAX = range;
 	cout << "Autoclicker ready." << endl << "Left Click: R" << endl << "Right Click: F";
-		while (1) {
-			if (GetAsyncKeyState(0x52)) {
-				mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-				mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-			}
-			if (GetAsyncKeyState(0x46)) {
-				mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
-				mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
-			}
-			Sleep(1000 / (cpsmin + range));
+	while (1) {
+		if (GetAsyncKeyState(0x52)) {
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 		}
+		if (GetAsyncKeyState(0x46)) {
+			mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+		}
+		Sleep(1000 / (cpsmin + range));
+	}
 }
